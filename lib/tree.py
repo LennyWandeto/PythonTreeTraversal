8433a8afa1af66a49c5d16ec7f8addd0fe54c293
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-06-15 15:00:35 +0300

    completed

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,11 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
+    allnodes = [self.root]
+    while allnodes:
+      node = allnodes.pop()
+      if node['id'] == id:
+        return node
+      allnodes = allnodes + node['children']
+    return None
     pass
